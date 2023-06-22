73f8244465519f5c6d8dbf868d36173fb8af259e
Felipe Sales <felipe.ferreira.sales@gmail.com>
2023-06-22 09:56:25 -0300

    Cria tabelas pedido e checkout

--- src/main/kotlin/br/com/fiap/postech/fastfood/adapter/outbound/infrastructure/database/entities/CheckoutEntity.kt
+++ src/main/kotlin/br/com/fiap/postech/fastfood/adapter/outbound/infrastructure/database/entities/CheckoutEntity.kt
@@ -1,6 +1,5 @@
 package br.com.fiap.postech.fastfood.adapter.outbound.infrastructure.database.entities
 
-import br.com.fiap.postech.fastfood.application.domain.models.Pedido
 import jakarta.persistence.*
 import jakarta.persistence.CascadeType.ALL
 import java.util.*
@@ -14,7 +13,7 @@ data class CheckoutEntity (
 
     @OneToOne(cascade = arrayOf(ALL))
     @JoinColumn(name = "pedido_id", referencedColumnName = "id")
-    var pedido: Pedido,
+    var pedido: PedidoEntity,
 
     @Column(name = "status")
     var status: String
