b91e6788687d98550f0b4708899b834ba3cd9b96
Felipe Sales <felipe.ferreira.sales@gmail.com>
2023-06-29 08:14:23 -0300

    Correção da abordagem do checkout

--- src/main/kotlin/br/com/fiap/postech/fastfood/application/domain/models/Checkout.kt
+++ src/main/kotlin/br/com/fiap/postech/fastfood/application/domain/models/Checkout.kt
@@ -1,12 +1,14 @@
 package br.com.fiap.postech.fastfood.application.domain.models
 
+import br.com.fiap.postech.fastfood.application.domain.valueObjets.FormaPagamento
 import br.com.fiap.postech.fastfood.application.domain.valueObjets.StatusCheckout
 import java.time.LocalDateTime
 import java.util.*
 
 data class Checkout (
     var id: UUID? = null,
-    var pedido: Pedido,
-    var status: StatusCheckout,
-    var data: LocalDateTime
+    var pedido: Pedido? = null,
+    var status: StatusCheckout? = null,
+    var formaPagamento: FormaPagamento? = null,
+    var data: LocalDateTime? = null
 )
