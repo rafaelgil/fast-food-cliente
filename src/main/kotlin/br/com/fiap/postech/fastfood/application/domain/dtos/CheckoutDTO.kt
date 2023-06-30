3874a657431d6dc988da2f9fce44ab4d38d901a4
Felipe Sales <felipe.ferreira.sales@gmail.com>
2023-06-29 23:55:46 -0300

    Revert "Dtos Controllers"
    This reverts commit 576ec4d97eae2fbe57643f9888608d011ef566ea.

--- src/main/kotlin/br/com/fiap/postech/fastfood/application/domain/dtos/CheckoutDTO.kt
+++ src/main/kotlin/br/com/fiap/postech/fastfood/application/domain/dtos/CheckoutDTO.kt
@@ -15,3 +15,12 @@ data class CheckoutDTO(
     var formaPagamento: FormaPagamento? = null,
     var data: LocalDateTime? = null
 )
+
+data class CheckoutRequest (
+
+    @JsonProperty("id_pedido")
+    var idPedido: UUID? = null,
+
+    @JsonProperty("forma_pagamento")
+    var formaPagamento: String? = null
+)
