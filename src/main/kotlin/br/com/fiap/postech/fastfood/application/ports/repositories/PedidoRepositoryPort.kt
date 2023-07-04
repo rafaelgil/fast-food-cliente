1494a2e89fb9ec709b1a599aa2df579cc9b5d9d8
Wanderson <contato@wandersoncesar.com.br>
2023-07-03 21:10:14 -0300

    Alteracao no pedido

--- src/main/kotlin/br/com/fiap/postech/fastfood/application/ports/repositories/PedidoRepositoryPort.kt
+++ src/main/kotlin/br/com/fiap/postech/fastfood/application/ports/repositories/PedidoRepositoryPort.kt
@@ -2,13 +2,14 @@ package br.com.fiap.postech.fastfood.application.ports.repositories
 
 import br.com.fiap.postech.fastfood.adapter.outbound.infrastructure.database.entities.PedidoEntity
 import br.com.fiap.postech.fastfood.application.domain.dtos.PedidoDTO
+import br.com.fiap.postech.fastfood.application.domain.dtos.PedidoResponseDTO
 import br.com.fiap.postech.fastfood.application.domain.models.Pedido
 import java.util.*
 
 interface PedidoRepositoryPort {
 
-    fun cadastrar(pedido: Pedido): PedidoDTO
-    fun atualizar(pedido: Pedido): PedidoDTO
+    fun cadastrar(pedido: Pedido): PedidoResponseDTO
+    fun atualizar(pedido: Pedido): PedidoResponseDTO
     fun listar(): List<Pedido>
     fun busca(id: UUID): Optional<Pedido>
 }
